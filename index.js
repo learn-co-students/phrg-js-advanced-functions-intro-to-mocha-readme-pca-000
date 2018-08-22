79c233f3305da7cffbfc61e201724823c8682ffc
Daniel Mack <daniel.mack@powerhrg.com>
2018-08-22 09:58:11 -0400

    finished lab

---
+++ index.js
@@ -0,0 +1,24 @@
+function favoriteIceCream(flavor) {
+  flavor = "mint chocolate chip"
+  return `I love ${flavor}`
+}
+
+function theTruth() {
+  return true
+}
+
+function sayHey() {
+  return "Hey!"
+}
+
+function sayHeyFriend(name) {
+  return `Hey, ${name}!`
+}
+
+function shouting() {
+  return "I LOVE JAVASCRIPT SOOOOOO MUCH"
+}
+
+function roundDown() {
+  return Math.floor(5.1)
+}
