f29ff18d4a8cd8d0d05af0bbf12ac1134f3214f9
Lauren Abrams <lauren.abrams@powerhrg.com>
2018-08-26 19:25:43 -0400

    finished intro to mocha readme

---
+++ index.js
@@ -0,0 +1,23 @@
+function theTruth() {
+  return true
+}
+
+function sayHey(){
+  return "Hey!"
+}
+
+function sayHeyFriend(name) {
+  return `Hey, ${name}!`
+}
+
+function favoriteIceCream(flavor){
+  return `I love ${flavor}`
+}
+
+function shouting() {
+  return "I LOVE JAVASCRIPT SOOOOOO MUCH"
+}
+
+function roundDown() {
+  return Math.floor(5.0003)
+}
